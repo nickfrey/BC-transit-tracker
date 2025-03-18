124edbe73c8f44d8c7a2cfa96b76e9f067400b78
TJ Horner <me@tjhorner.com>
2025-03-17 20:21:30 -0700

    allow deep links to configurator

--- docs/src/pages/configurator/index.tsx
+++ docs/src/pages/configurator/index.tsx
@@ -1,11 +1,18 @@
 import Layout from "@theme/Layout"
-import { ReactNode } from "react"
+import { ReactNode, useEffect, useRef } from "react"
 
 export default function Configurator(): ReactNode {
+  const destinationPath = window.location.hash.replace("#!", "")
+
+  const src = new URL("https://countdown-config.horner.tj")
+  src.pathname = destinationPath
+
+  window.location.hash = ""
+
   return (
     <Layout title="Configurator" wrapperClassName="full-height">
       <iframe
-        src="https://countdown-config.horner.tj"
+        src={src.href}
         allow="serial; usb; bluetooth; clipboard-write; clipboard-read; geolocation"
         style={{
           height: "100%",
